cd4fe7388a7a60701645799747d06a71da68a2a1
Psihey <psihey1@gmail.com>
2019-06-26 23:11:29 +0300

    Added Security

---
+++ src/main/java/com/restTracker/restTracker/service/JwtUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.restTracker.restTracker.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 26.06.2019.
+ */
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        if ("javainuse".equals(s)) {
+            return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + s);
+        }
+    }
+}
